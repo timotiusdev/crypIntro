8c173e170d83e9c9a7301fbcf6e9176fd6a8b89b
Timotius Devin <timotiusdevin@live.com>
2017-03-13 13:03:33 +0700

    Update jacobiOnCPP.cpp
    Add negative number handler and updated to long int

--- jacobiOnCPP.cpp
+++ jacobiOnCPP.cpp
@@ -1,49 +1,53 @@
 #include <iostream>
-
+#include <stdlib.h>
+#include <cmath>
 using namespace std;
 
-int jacobi(int, int);
+long jacobi(long a, long n) {
+    if(a <= 1)
+	return a;
+    else if (a%2 == 1){
+	if((a%4 == 3) && (n%4 == 3))
+	    return (-1 * jacobi (n%a,a));
+	else
+	    return jacobi (n%a,a);
+    }
+    else{
+	if((n%8 == 3) || (a%8 == 5))
+	    return (-1 * jacobi (a/2, n));
+	else
+	    return jacobi (a/2, n);
+    }
+}
+
+long findResidue(long a, long n){
+    for(long x=0; x<((n/2)+2); x++){
+        if(((int)pow(x,2)%n)==a)
+            return x;
+    }
+}
 
 int main()
 {
-    int a, n;
+    long a, n, result ;
 
     cout << "Value of a = ";
     cin >> a;
     cout << "Value of n = ";
     cin >> n;
 
-    cout << "\nThe Jacobi symbol = " << jacobi(a,n) << endl;
+    /* negative number handler */
+    if (a <= 0)
+	a = a%n + n;
+    
+    result = jacobi(a,n);
+
+
+
+	cout << "\nThe Jacobian symbol of Jacobi("<< a <<", "<< n <<") is " << result << endl;
+
+    if (jacobi (a,n) == 1)
+	cout <<"Value of quadratic residue is " << findResidue(a, n) << endl;	
 
     return 0;
-}
-
-/* a, n >= 0 AND n is odd */
-int jacobi(int a, int n) {
-    int ans;
-
-/*check the value of a AND n*/
-    if (a == 0)
-        ans = (n == 1) ? 1 : 0;
-/* check the remains of a mod 8 IF a == 2 */
-    else if (a == 2) {
-        switch ( n % 8 ) {
-            case 1:
-            case 7:
-                    ans = 1;
-                    break;
-            case 3:
-            case 5:
-                    ans = -1;
-                    break;
-        }
-    }
-/* IF a >= n */
-    else if ( a >= n )
-        ans = jacobi(a%n, n);
-    else if ( a % 2 == 0 )
-        ans = jacobi(2,n)*jacobi(a/2, n);
-    else
-        ans = ( a % 4 == 3 && n % 4 == 3 ) ? -jacobi(n,a) : jacobi(n,a);
-    return ans;
 }
